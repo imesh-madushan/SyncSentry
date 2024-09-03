09e844dd4ce85272d505bf620a730e79a66971df
imeshme <imeshmadushan927@gmail.com>
2024-09-03 12:42:57 +0530

    db query text uploaded and creadintails changed to localhost

--- src/com/ss/Database/DbConnection.java
+++ src/com/ss/Database/DbConnection.java
@@ -9,9 +9,9 @@ import java.sql.Statement;
 class DbConnection{
     private Connection dbCon;
     private Statement cmdSql;
-    private final String dbUrl = "jdbc:mysql://dbhost:3306"; //change this with your actual database host url and port
-    private final String dbUser = "dbusername"; //change the username with your credintials
-    private final String dbPasswd = "dbpassword"; //change the password as well
+    private final String dbUrl = "jdbc:mysql://localhost:3306/syncsentry"; //change this with your actual database host url and port
+    private final String dbUser = "root"; //change the username with your credintials
+    private final String dbPasswd = ""; //change the password as well
 
     protected DbConnection(){
     }
